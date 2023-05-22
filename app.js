1c1222b3d6c264210197387254ce199d905332f4
Angel Roberto Andrade <alroc1234@hotmail.com>
2023-05-22 01:22:50 -0700

    boilerplate html with button and title

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+import randomWords from "random-words";
+
+/**
+ * IMPORTANT RUN THROUGH VITE OR IT MAY FUCK UP EVERYTHING
+ * IT WON'T WORK IF YOU DON'T RUN IT THROUGH VITE
+ */
+const chosenWord = randomWords(1);
