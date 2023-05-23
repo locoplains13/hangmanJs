e2aa5bd07e2aa83fbe9e5a7b83fc02c4a1485763
Angel Roberto Andrade <alroc1234@hotmail.com>
2023-05-22 19:34:04 -0700

    displayed hidden word with underscores

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ import randomWords from "random-words";
  * IT WON'T WORK IF YOU DON'T RUN IT THROUGH VITE
  */
 const chosenWord = randomWords(1);
+let hiddenWord = "";
+
+console.log(chosenWord[0].length);
+
+for (let i = 0; i < chosenWord[0].length; i++) {
+  hiddenWord += " _ ";
+}
+document.getElementById("main-game").textContent = hiddenWord;
