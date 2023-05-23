0302cd1fdf212d4ea171df60b820955dee50db0d
Angel Roberto Andrade <alroc1234@hotmail.com>
2023-05-22 21:25:48 -0700

    hangman works, displays word properly at guessing

--- app.js
+++ app.js
@@ -5,6 +5,64 @@ import randomWords from "random-words";
  * IMPORTANT RUN THROUGH VITE OR IT MAY FUCK UP EVERYTHING
  * IT WON'T WORK IF YOU DON'T RUN IT THROUGH VITE
  */
+
+// first generation of the word
+const chosenWord = randomWords(1)[0];
+
+//the word but to be displayed as hidden
+let hiddenWord = hideWord(chosenWord);
+displayWord();
+
+function setMessage(message) {
+  document.getElementById("message").textContent = message;
+}
+
+function checkIfEqual(guess, chosenWord, hiddenWord) {
+  let foundLetter = false;
+  let message = "you can't guess an empty space";
+  if (guess === chosenWord) {
+    hiddenWord = guess;
+    revealWord();
+  } else if (guess === "") {
+    setMessage(message);
+  } else {
+    for (let i = 0; i < chosenWord.length; i++) {
+      if (chosenWord[i] === guess) {
+        hiddenWord[i] = guess; // FIXME FIX THIS WHOLE SHITTY ASS FUNCTION
+        displayWord();
+        setMessage("you got a letter right");
+        foundLetter = true;
+      } else if (!foundLetter) {
+        setMessage("wrong letter");
+      }
+    }
+  }
+}
+
+function hideWord(chosenWord) {
+  let hiddenWord = new Array();
+  for (let i = 0; i < chosenWord.length; i++) {
+    hiddenWord[i] = "_";
+  }
+  return hiddenWord;
+}
+
+function displayWord() {
+  let newHiddenWord = "";
+  for (let i = 0; i < hiddenWord.length; i++) {
+    newHiddenWord += hiddenWord[i] + " ";
+    document.getElementById("main-game").textContent = newHiddenWord;
+  }
+}
+
+function revealWord() {
+  let newHiddenWord = "";
+  for (let i = 0; i < chosenWord.length; i++) {
+    newHiddenWord += chosenWord[i] + " ";
+    document.getElementById("main-game").textContent = newHiddenWord;
+  }
+}
+
 function draw() {
   const canvas = document.querySelector("#canvas");
 
@@ -25,12 +83,11 @@ function draw() {
 }
 draw();
 
-const chosenWord = randomWords(1);
-let hiddenWord = "";
+console.log(chosenWord.length);
 
-console.log(chosenWord[0].length);
+console.log(chosenWord);
 
-for (let i = 0; i < chosenWord[0].length; i++) {
-  hiddenWord += " _ ";
-}
-document.getElementById("main-game").textContent = hiddenWord;
+document.getElementById("guess-button").addEventListener("click", function () {
+  let guess = document.getElementById("guess").value;
+  checkIfEqual(guess, chosenWord, hiddenWord);
+});
