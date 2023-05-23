272d9a5dcaff91c1e2313dac7fe13be613b288ad
Angel Roberto Andrade <alroc1234@hotmail.com>
2023-05-22 19:48:54 -0700

    added canvas element to page to draw hangman

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ import randomWords from "random-words";
  * IMPORTANT RUN THROUGH VITE OR IT MAY FUCK UP EVERYTHING
  * IT WON'T WORK IF YOU DON'T RUN IT THROUGH VITE
  */
+function draw() {
+  const canvas = document.querySelector("#canvas");
+
+  if (!canvas.getContext) {
+    return;
+  }
+  const ctx = canvas.getContext("2d");
+
+  // set line stroke and line width
+  ctx.strokeStyle = "red";
+  ctx.lineWidth = 5;
+
+  // draw a red line
+  ctx.beginPath();
+  ctx.moveTo(100, 100);
+  ctx.lineTo(300, 100);
+  ctx.stroke();
+}
+draw();
+
 const chosenWord = randomWords(1);
 let hiddenWord = "";
 
